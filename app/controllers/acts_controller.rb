3308937e22937243e11c4e7140681cbfd15f9bd3
Kate <katepdonahue@gmail.com>
2014-01-09 16:28:49 -0500

    Add actions for act controller

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -1,31 +1,35 @@
 class ActsController < ApplicationController
 
   def index
-
+    @acts = Act.all
   end
 
   def new
-
+    @act = Act.new
   end
 
   def create
-
+    @act = Act.create(params[:act])
+    redirect_to @act
   end
 
   def show
-
+    @act = Act.find(params[:id])
   end
 
   def edit
-
+    @act = Act.find(params[:id])
   end
 
   def update
-
+    @act = Act.find(params[:id])
+    @act.update_attributes(params[:act])
+    redirect_to @act
   end
 
   def destroy
-
+    Act.find(params[:id]).destroy
+    redirect_to '/acts'
   end
 
 end
