47f781e5074042d31ea2d6979bae92e647b6bc0d
Kate <katepdonahue@gmail.com>
2014-01-09 15:22:04 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmoebaCircus::Application.config.secret_token = 'e16bdadaf9a88f00c3c9f6efcb8b72d5d1c8aa8a7ce5c64c2f1588fda907eb577a192d0d50d66c69b581f1acbf1d45ba0629a3e55106867673e4bc135d91e753'
