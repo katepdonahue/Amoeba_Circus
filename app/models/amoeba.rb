5414ee10db398b701d433bde43b5fc6360805b70
Kate <katepdonahue@gmail.com>
2014-01-10 11:53:35 -0500

    Add collection select for talents in new amoebas view

--- app/models/amoeba.rb
+++ app/models/amoeba.rb
@@ -1,5 +1,5 @@
 class Amoeba < ActiveRecord::Base
-  attr_accessible :name, :generation, :talent_id, :act_id
+  attr_accessible :name, :generation, :talent_id
   belongs_to :talent
   has_many :acts, :through => :act_amoebas
 end
