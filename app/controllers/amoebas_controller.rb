312e9c23c1a299099fb5a6a5cf1a28e4074d43ef
Kate <katepdonahue@gmail.com>
2014-01-10 11:16:06 -0500

    Add basic controllers for amoebas and talents based on acts

--- app/controllers/amoebas_controller.rb
+++ app/controllers/amoebas_controller.rb
@@ -1,31 +1,35 @@
 class AmoebasController < ApplicationController
 
   def index
-
+    @amoebas = Amoeba.all
   end
 
   def new
-
+    @amoeba = Amoeba.new
   end
 
   def create
-
+    @amoeba = Amoeba.create(params[:amoeba])
+    redirect_to @amoeba
   end
 
   def show
-
+    @amoeba = Amoeba.find(params[:id])
   end
 
   def edit
-
+    @amoeba = Amoeba.find(params[:id])
   end
 
   def update
-
+    @amoeba = Amoeba.find(params[:id])
+    @amoeba.update_attributes(params[:amoeba])
+    redirect_to @amoeba
   end
 
   def destroy
-
+    Amoeba.find(params[:id]).destroy
+    redirect_to '/amoebas'
   end
 
 
