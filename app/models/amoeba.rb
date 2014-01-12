3ff192b7c08bf96087ad64e71cf3d4336ff6db90
Kate <katepdonahue@gmail.com>
2014-01-12 18:14:03 -0500

    Create naming methods, also, don't really need split to return anything

--- app/models/amoeba.rb
+++ app/models/amoeba.rb
@@ -4,14 +4,20 @@ class Amoeba < ActiveRecord::Base
   has_many :act_amoebas
   has_many :acts, :through => :act_amoebas
 
+  def jr1
+    origin = self.name
+    "#{origin[0].next}#{origin[1..-1]}ff"
+  end
+
+  def jr2
+    origin = self.name
+    "#{origin[0].next.next}#{origin[1..-1]}ss"
+  end
+
   def split
-    children = []
-    2.times do
-      child = Amoeba.create(:name=>self.name, :generation=>(self.generation+1), :talent_id=>self.talent_id)
-      children << child
-    end
+    Amoeba.create(:name=>self.jr1, :generation=>(self.generation+1), :talent_id=>self.talent_id)
+    Amoeba.create(:name=>self.jr2, :generation=>(self.generation+1), :talent_id=>self.talent_id)
     self.destroy
-    children
   end
 
 end
