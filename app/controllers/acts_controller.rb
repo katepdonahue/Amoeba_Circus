12b1ea610aa81fd0d590d23fdcce11fad6392f95
Kate <katepdonahue@gmail.com>
2014-01-12 16:42:25 -0500

    Update create action to assign attributes one at a time

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -9,7 +9,15 @@ class ActsController < ApplicationController
   end
 
   def create
-    @act = Act.create(params[:act])
+    @act = Act.new
+    p = params[:act]
+    @act.name = p[:name]
+    @act.date = Date.parse("#{p["date(3i)"]}/#{p["date(2i)"]}/#{p["date(1i)"]}")
+    @act.time = Time.parse("#{p["time(4i)"]}:#{p["time(5i)"]}")
+    p[:amoebas][1..-1].each do |amoeba_id|
+      @act.amoebas << Amoeba.find(amoeba_id)
+    end
+    @act.save
     redirect_to @act
   end
 
