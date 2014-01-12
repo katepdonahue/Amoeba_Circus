433bd9db448fecb9c4cc57f270102fcabc08c429
Kate <katepdonahue@gmail.com>
2014-01-12 16:21:40 -0500

    More of same refactor

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -23,11 +23,11 @@ class ActsController < ApplicationController
 
   def update
     @act = Act.find(params[:id])
-    @act.name = params[:act][:name]
     p = params[:act]
+    @act.name = p[:name]
     @act.date = Date.parse("#{p["date(3i)"]}/#{p["date(2i)"]}/#{p["date(1i)"]}")
     @act.time = Time.parse("#{p["time(4i)"]}:#{p["time(5i)"]}")
-    params[:act][:amoebas].each do |amoeba_id|
+    p[:amoebas].each do |amoeba_id|
       @act.amoebas << Amoeba.find(amoeba_id)
     end
     @act.save
